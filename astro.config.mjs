9c9cbdaa9e7a41fa548d2a0e43965e9dd499ad7f
RafaLopezZz <142743097+RafaLopezZz@users.noreply.github.com>
2026-03-12 07:38:09 +0100

    Configuración deploy a GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site : "https://rafalopezzz.github.io/website",
+  base : "/web-site",
   output: "static",
   integrations: [tailwind()]
 });
