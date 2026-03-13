95914e86e786ee8e8bbf3231a2d2f7a581365ec2
RafaLopezZz <142743097+RafaLopezZz@users.noreply.github.com>
2026-03-13 07:06:01 +0100

    Arreglado astro site/base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site : "https://rafalopezzz.github.io/website",
+  site : "https://rafalopezzz.github.io",
   base : "/web-site",
   output: "static",
   integrations: [tailwind()]
