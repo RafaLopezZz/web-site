d56fc9dd2b4a6c73bb98c0f3a56541566ed303ab
RafaLopezZz <142743097+RafaLopezZz@users.noreply.github.com>
2026-03-15 19:19:02 +0100

    Solucionado error de visualización de certificados

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -5,25 +5,25 @@ export const certificates: Certificate[] = [
     title: "Bash/Shell",
     issuer: "Mouredev PRO",
     date: "2026",
-    file: "/certificates/Certificado BASH - Rafael López - 2026-01-11.pdf",
+    file: "/web-site/certificates/Certificado BASH - Rafael López - 2026-01-11.pdf",
   },
     {
     title: "Curso de inicialización al desarrollo con IA",
     issuer: "BIG School - Mouredev",
     date: "2025",
-    file: "/certificates/Certificado_IA.pdf",
+    file: "/web-site/certificates/Certificado_IA.pdf",
   },
   {
     title: "Aspectos básicos de la asistencia técnica",
     issuer: "Coursera - Google",
     date: "2022",
-    file: "/certificates/Certificado Coursera W38TVAPHFLSL.pdf",
+    file: "/web-site/certificates/Certificado Coursera W38TVAPHFLSL.pdf",
   },
   {
     title: "Los bits y bytes de las redes informáticas",
     issuer: "Coursera - Google",
     date: "2022",
-    file: "/certificates/Certificado2 Coursera 9KWEUX3HNPQM.pdf",
+    file: "/web-site/certificates/Certificado2 Coursera 9KWEUX3HNPQM.pdf",
   },
 
 ];
